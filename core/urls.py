6ee4c7c1b5aa18aee163d3d969d03d2cd03133d8
ZARIFMAC <uzbzarif@gmail.com>
2023-02-28 10:17:37 +0500

    json response

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 from django.http import HttpResponse,HttpRequest,JsonResponse
 def index(request:HttpRequest):
     
-    return HttpResponse("<h1>Home page</h1>")
+    return JsonResponse({
+        'result':True
+    })
 
 urlpatterns = [
     path('', index),
