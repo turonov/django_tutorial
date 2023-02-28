5a6497ab120ae5ab8d56eb8c9691510aeae6d33b
ZARIFMAC <uzbzarif@gmail.com>
2023-02-28 11:14:12 +0500

    url

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,7 @@
 
 
 from django.urls import path
-from django.http import HttpResponse,HttpRequest,JsonResponse
-
-def index(request:HttpRequest):
-    data=request.GET
-    a = int(data.get('a',0))
-    b = int(data.get('b',0))
-    return JsonResponse({'sum':a+b})
+from api.views import index
 
 
 urlpatterns = [
