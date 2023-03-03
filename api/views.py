fa0c839a0c9dfb5eab00c6aa94811fe7b81089f0
turonov <turonov5400gmail.com>
2023-03-03 13:55:40 +0500

    POST reqsts

--- api/views.py
+++ api/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse,HttpRequest,JsonResponse
-
+import json
 def index(request:HttpRequest):
     data=request.GET
     a = int(data.get('a',0))
     b = int(data.get('b',0))
     return JsonResponse({'sum':a+b})
+
+def get_data(request:HttpRequest):
+
+    if request.method == "POST":
+        data = request.body.decode()
+        print(json.loads(data))
+
+    if request.method =="GET":
+        print('GET')
+
+    return JsonResponse("hello world")
--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,9 @@
 
 from django.urls import path
 from api.views import index
-
+from api.views import get_data
 
 urlpatterns = [
     path('', index),
+    path('get_data/',get_data)
 ]
